9dac7014556b4a61a294b0e25952b74a081ac0fc
Suriyaa <suriyaaap13@gmail.com>
2021-07-14 17:27:48 +0530

    Authentication steps such as 1.Create User 2.Create Session 3.Show Details of signed in user 4.sign-out

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -2,9 +2,39 @@ const User = require('../models/user');
 
 //render the profile page
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title: "User Profile"
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(err){
+                console.log('Error in finding the user id');
+                return res.redirect('/users/sign-in');
+            }
+
+            if(user){
+                return res.render('user_profile',{
+                    title: "User Profile",
+                    user: user
+                });
+            }
+
+            return res.redirect('/users/sign-in');
+
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
+}
+
+//creating the logout session
+module.exports.logout = function(req,res){
+    if(req.cookies.user_id){
+        User.findByIdAndDelete(req.cookies.user,function(err){
+            if(err){
+                console.log('Error in logging out the user');
+                return res.redirect('/users/sign-in');
+            }
+        });
+    }
+    return res.redirect('/users/sign-in');
 }
 
 //render the sign-in page
@@ -50,5 +80,28 @@ module.exports.create = function(req,res){
 
 //get the sign-in data and create-session for the user
 module.exports.createSession = function(req,res){
-    //TODO Later
+
+    //steps for authentication
+    //find the user
+    User.findOne({email: req.body.email},function(err,user){
+
+        if(err){
+            console.log('Error in signing in the user');
+        }
+
+        if(user){
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+            
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');  
+        }else{
+            console.log('user doesnot exist');
+            return res.redirect('back');
+        }
+
+        
+
+    });
 }
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,10 @@ const userController = require('../controllers/user_controller');
 router.get('/profile',userController.profile);
 router.get('/sign-in',userController.signIn);
 router.get('/sign-up',userController.signUp);
+router.get('/logout',userController.logout);
 router.post('/create',userController.create);
 router.post('/create-session',userController.createSession);
+
 
 console.log('user router is working');
 module.exports = router;
